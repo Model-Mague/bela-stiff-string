03c9386d615908b554e7223108e088fc4131bfc1
Justas Bikulcius <just.bikulcius@gmail.com>
2023-03-07 01:21:51 +0000

    Fix some bugs, add DESKTOP_BUILD def

--- Main.cpp
+++ Main.cpp
@@ -21,18 +21,20 @@ extracted from: https://www2.ph.ed.ac.uk/~sbilbao/matlabpage.html
 #include <array>
 #include "Eigen/Sparse"
 #include "Eigen/LU"
-void export_matrix(Eigen::MatrixXf mx, std::string path);
 
-float inharmonicity = 0.001;                                 // inharmonicity parameter (>0)
-float f0 = 100;                                  // fundamental(Hz)
-float TF = 2;                                    // duration of simulation(s)
-float ctr = 0.1;
-float wid = 0.05;                   			 // center location/width of excitation
-float u0 = 1;
-float v0 = 0;                           		 // maximum initial displacement/velocity
-float rp[2] = { 0.3, 0.7 };                        // positions of readout(0-1)
-float loss[2][2] = { {100, 10}, {1000, 8} };       // loss [freq.(Hz), T60(s), freq.(Hz), T60(s)]
-float theta = 1.0;
+#define DESKTOP_BUILD
+
+
+float inharmonicity = 0.001f;                                 // inharmonicity parameter (>0)
+float f0 = 100.f;                                  // fundamental(Hz)
+float TF = 2.f;                                    // duration of simulation(s)
+float ctr = 0.1f;
+float wid = 0.05f;                   			 // center location/width of excitation
+float u0 = 1.f;
+float v0 = 0.f;                           		 // maximum initial displacement/velocity
+float rp[2] = { 0.3f, 0.7f };                        // positions of readout(0-1)
+float loss[2][2] = { {100.f, 10.f}, {1000.f, 8.f} };       // loss [freq.(Hz), T60(s), freq.(Hz), T60(s)]
+float theta = 1.0f;
 float SR;
 
 float NF;
@@ -61,11 +63,16 @@ Eigen::MatrixXf u;
 Eigen::MatrixXf out;
 
 
+void export_matrix(Eigen::MatrixXf mx, const std::string& path)
+{
+	std::ofstream out(path);
+	out << mx;
+}
 
 
 Eigen::MatrixXf toeplitz(Eigen::VectorXf r, Eigen::MatrixXf zeros_1d_matrix)
 {
-	int size = r.size() + zeros_1d_matrix.cols();
+	const auto size = r.size() + zeros_1d_matrix.cols();
 	Eigen::MatrixXf out(size, size);
 
 	for (int i = 0; i < size; i++)
@@ -127,7 +134,12 @@ Eigen::VectorXf gen_consecutive_vec(int from, int to)
 	return result;
 }
 
+
+#ifdef DESKTOP_BUILD
 bool setup()
+#else
+bool setup(BelaContext* context, void* userData)
+#endif
 {
 	Eigen::MatrixXd m(2, 2);
 	m(0, 0) = 3;
@@ -140,7 +152,7 @@ bool setup()
 	NF = floor(SR * TF);
 	k = 1 / SR;                                  // time step
 	gumo = 2 * f0;
-	K = sqrt(inharmonicity) * (gumo / M_PI);      // set parameters
+	K = sqrt(inharmonicity) * (gumo / (float)M_PI);      // set parameters
 
 	// stability conditions
 
@@ -165,13 +177,13 @@ bool setup()
 	float gammaSq2 = powf(gumo, 2);
 	float gammaSq4 = powf(gumo, 4);
 	float Ksq2 = powf(K, 2);
-	float piLoss00 = powf(2 * M_PI * loss[0][0], 2);
-	float piLoss10 = powf(2 * M_PI * loss[1][0], 2);
+	float piLoss00 = powf(2 * (float)M_PI * loss[0][0], 2);
+	float piLoss10 = powf(2 * (float)M_PI * loss[1][0], 2);
 
 	zeta1 = (-gammaSq2 + sqrt(gammaSq4 + 4 * Ksq2 * piLoss00)) / (2 * Ksq2);
 	zeta2 = (-gammaSq2 + sqrt(gammaSq4 + 4 * Ksq2 * piLoss10)) / (2 * Ksq2);
-	sig0 = 6 * log(10) * (-zeta2 / loss[0][1] + zeta1 / loss[1][1]) / (zeta1 - zeta2);
-	sig1 = 6 * log(10) * (1 / loss[0][1] - 1 / loss[1][1]) / (zeta1 - zeta2);
+	sig0 = 6 * (int)log(10) * (-zeta2 / loss[0][1] + zeta1 / loss[1][1]) / (zeta1 - zeta2);
+	sig1 = 6 * (int)log(10) * (1 / loss[0][1] - 1 / loss[1][1]) / (zeta1 - zeta2);
 
 
 	// create update matrices
@@ -186,8 +198,8 @@ bool setup()
 	// Total dimension 2 + N - 3 = N - 1
 	// Matrix element count (N-1) * (N-1)
 
-	Eigen::MatrixXf zerosNMinus3 = zeros(1, N - 3);
-	Eigen::MatrixXf zerosNMinus4 = zeros(1, N - 4);
+	Eigen::MatrixXf zerosNMinus3 = zeros(1, (int)N - 3);
+	Eigen::MatrixXf zerosNMinus4 = zeros(1, (int)N - 4);
 
 
 	// M = sparse(toeplitz([theta (1-theta)/2 zeros(1,N-3)]));
@@ -195,7 +207,7 @@ bool setup()
 	mComponent(0) = theta;
 	mComponent(1) = (1 - theta) / 2;
 
-	Eigen::MatrixXf M = toeplitz(mComponent, zerosNMinus3); // @HERE Does this really return a matrix of elementCountMinus elements?  Function returns 25 here
+	Eigen::MatrixXf M = toeplitz(mComponent, zerosNMinus3);
 
 	Eigen::VectorXf aComponent(2);
 	aComponent(0) = sig1 * k / (powf(h, 2)) + sig0 * k / 2;
@@ -214,7 +226,6 @@ bool setup()
 	cComponent(0) = -sig1 * k / (powf(h, 2)) - sig0 * k / 2;
 	cComponent(1) = sig1 * k / (2 * powf(h, 2));
 	C = toeplitz(cComponent, zerosNMinus3);
-	std::cout << cComponent;
 	// C = M + C
 	C = M + C;
 
@@ -231,25 +242,25 @@ bool setup()
 
 	// Create raised cosine
 	// xax = [1:N-1]'*h;
-	Eigen::VectorXf xax = gen_consecutive_vec(1, N); // maybe needs tanspose here
-	Eigen::VectorXf xaxMinusWid = gen_consecutive_vec(1, N);
-	Eigen::VectorXf xaxPlusWid = gen_consecutive_vec(1, N);
-	Eigen::VectorXf rc = gen_consecutive_vec(1, N);
+	Eigen::VectorXf xax = gen_consecutive_vec(1, (int)N); // maybe needs tanspose here
+	Eigen::VectorXf xaxMinusWid = gen_consecutive_vec(1, (int)N);
+	Eigen::VectorXf xaxPlusWid = gen_consecutive_vec(1, (int)N);
+	Eigen::VectorXf rc = gen_consecutive_vec(1, (int)N);
 
-	for (int i = 0; i < N - 1; i++) {
+	for (int i = 0; i < (int)N - 1; i++) {
 		xax(i) = xax(i) * h;
 		// Copy values into soon-to-be-used matrices
 		xaxMinusWid(i) = xax(i) - ctr - wid / 2;
 		xaxPlusWid(i) = xax(i) - ctr + wid / 2;
 
-		rc(i) = 1 + cos(2 * M_PI * (xax(i) - ctr) / wid);
+		rc(i) = 1 + (float)cos(2 * M_PI * (xax(i) - ctr) / wid);
 	}
 
 	int n_as_int = (int)roundf(N);
 	// ind = sign(max(-(xax-ctr-wid/2).*(xax-ctr+wid/2),0));
 	Eigen::VectorXf ind(n_as_int - 1);
 	// Multiply element-wise (xax-ctr-wid/2).*(xax-ctr+wid/2)
-	for (int i = 0; i < N - 1; i++)	ind(i) = -1 * xaxMinusWid(i) * xaxPlusWid(i);
+	for (int i = 0; i < (int)N - 1; i++)	ind(i) = -1 * xaxMinusWid(i) * xaxPlusWid(i);
 
 	export_matrix(ind, "premax.txt");
 	max_set(ind, 0);
@@ -257,7 +268,7 @@ bool setup()
 	sign_set(ind);
 
 	// rc = 0.5*ind.*(rc);
-	for (int i = 0; i < N - 1; i++) rc(i) = 0.5 * ind(i) * rc(i);
+	for (int i = 0; i < (int)N - 1; i++) rc(i) = 0.5f * ind(i) * rc(i);
 
 	/*
 		u2 = u0*rc;
@@ -275,8 +286,8 @@ bool setup()
 	// @HERE Should be u1 = (u0 + k * v0) * rc; but check how to do this in Eigen
 	//for (int i = 0; i < N - 1; i++) u1[i] = (u0 + k * v0) * rc[i]; // @HERE was bug, u1[i]
 
-	u = zeros(N + 1, 1);
-	out = zeros(NF, 2);
+	u = zeros((int)N + 1, 1);
+	out = zeros((int)NF, 2);
 
 	export_matrix(u1, "u1.txt");
 	export_matrix(u2, "u2.txt");
@@ -291,52 +302,45 @@ bool setup()
 	return true;
 }
 
-void export_matrix(Eigen::MatrixXf mx, std::string path)
-{
-	std::ofstream out(path);
-	out << mx;
-}
 
-
+#ifdef DESKTOP_BUILD
 int main(int argc, char** argv)
+#else
+void render(BelaContext* context, void* userData)
+#endif
 {
+#ifdef DESKTOP_BUILD
 	setup();
-	for (unsigned int n = 0; n < 10; n++)
+#endif
+
+	int first_index = (int)roundf(rp_int(0));
+	int second_index = (int)roundf(rp_int(1));
+
+	Eigen::Vector2f urpint;
+	Eigen::Vector2f urpintplus;
+
+	//auto urpintT = urpint.transpose();
+	//auto urpintplusT = urpintplus.transpose();
+
+	Eigen::Vector2f rp_frac_minus1 = Eigen::Vector2f::Ones(2) - rp_frac;
+
+	for (unsigned int n = 0; n < 20; n++)
 	{
 		Eigen::MatrixXf Bmember = B * u1 - C * u2;
 		Eigen::VectorXf u = A.lu().solve(Bmember);
-		//export_matrix(A, "A.txt");
-		//export_matrix(B, "B.txt");
-		//export_matrix(Bmember, "Bmember.txt");
-		//export_matrix(C, "C.txt");
-		//export_matrix(u1, "u1.txt");
-		//export_matrix(u2, "u2.txt");
-
-		// New vector
-		// First elements is u((int)roundf(rp_int(0))) second element is u((int)roundf(rp_int(1)))
-		int first_index = (int)roundf(rp_int(0));
-		int second_index = (int)roundf(rp_int(1));
-		Eigen::Vector2f urpint = u.cross(rp_int);
-		urpint.transpose();
-
-
-		auto urpintT = urpint; // tried removing T
-		//rp_int.array() += 1;
-
+		urpint(0) = u(first_index);
+		urpint(1) = u(second_index);
+		urpintplus(0) = u(first_index + 1);
+		urpintplus(1) = u(second_index + 1);
 		// (1-rp_frac).*u(rp_int)'+rp_frac.*u(rp_int+1)'
-		Eigen::Vector2f add1(1, 1);
-		Eigen::Vector2f urpintplus = u(rp_int + add1);
-		urpintplus.transpose();
-
-		auto urpintplusT = urpintplus; // tried removing T
-		Eigen::Vector2f out = (Eigen::Vector2f::Ones(2) - rp_frac).cwiseProduct(urpintT) + rp_frac.cwiseProduct(urpintplusT); // this is two numbers
-		export_matrix(out, "out.txt");
-		// @HERE UNCOMMENT THIS
-		//for (unsigned int channel = 0; context->audioOutChannels; channel++)
-		//{
-		//	audioWrite(context, n, channel, out(channel % 2));
-		//}
-		std::cout << out(0) << " " << out(1) << std::endl;
+		Eigen::Vector2f out = (rp_frac_minus1).cwiseProduct(urpint) + rp_frac.cwiseProduct(urpintplus); // this is two numbers
+#ifndef DESKTOP_BUILD
+		for (unsigned int channel = 0; context->audioOutChannels; channel++)
+		{
+			audioWrite(context, n, channel, out(channel % 2));
+		}
+#endif
+		std::cout << n << ": " << out(0) << " " << out(1) << std::endl;
 		// Update
 		u2 = u1;
 		u1 = u;
