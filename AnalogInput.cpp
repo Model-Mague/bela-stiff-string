038b029f6046fa66280180e94579b6cb1a8169e2
ptablas <125274411+ptablas@users.noreply.github.com>
2023-04-09 21:51:10 +0100

    Calibration!

--- AnalogInput.cpp
+++ AnalogInput.cpp
@@ -1,6 +1,26 @@
 #include "AnalogInput.h"
-
+//#include "Global.h"
 #include <cmath>
+#include <vector>
+
+static float limit(float val, float min, float max) // Global.h could not be found? Ended up re-defining
+    {
+        if (val < min)
+        {
+            val = min;
+            return val;
+        }
+        else if (val > max)
+        {
+            val = max;
+            return val;
+        }
+        return val;
+    }
+
+
+std::vector<float> upperlimitValue = {0.89, 0.89, 0.87, 0.89, 0.92, 0.92, 0.91, 0.92};
+std::vector<float> lowerlimitValue = {0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01};
 
 AnalogInput::AnalogInput(const std::string& label, const int channel, const std::pair<float, float>& valueRange, const float readThreshold)
 	: m_label(label), m_channel(channel), m_valueRange(valueRange), m_readThreshold(readThreshold),
@@ -15,6 +35,14 @@ void AnalogInput::read(BelaContext* context, const int frame)
 	{
 		m_hasChanged = true;
 		m_currentValue = read;
+		/* <-- I planned to update the values as I went but I have realized exactitude does not pay off.
+		if(read > upperlimitValue[m_channel])
+			upperlimitValue[m_channel] = read;
+			//rt_printf("updated channel %d upper limit with value %f\n" , m_channel, upperlimitValue[m_channel]);
+		if(read < lowerlimitValue[m_channel])
+			lowerlimitValue[m_channel] = read;
+			//rt_printf("updated channel %d lower limit with value %f\n" , m_channel, lowerlimitValue[m_channel]);
+		*/	
 	}
 	else
 	{
@@ -28,9 +56,10 @@ void AnalogInput::read(BelaContext* context, const int frame)
 		if (read < m_minValue)
 			m_minValue = read;
 	}
+	
 }
 
 float AnalogInput::getCurrentValueMapped() const
 {
-	return map(m_currentValue, 0.f, 1.f, m_valueRange.first, m_valueRange.second);
-}
+	return map(limit(m_currentValue, lowerlimitValue[m_channel], upperlimitValue[m_channel]), lowerlimitValue[m_channel], upperlimitValue[m_channel], m_valueRange.first, m_valueRange.second);
+} // Now incoming values are limited by imposed limits -> we loose an unperceptible amount of tweaking but guarantees the range limits are reachable
